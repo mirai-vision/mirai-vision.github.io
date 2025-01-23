4c143886789193661115642e81287b8b53908d05
suakaw <51227080+suakaw@users.noreply.github.com>
2025-01-23 06:16:16 +0000

    Deploying to gh-pages from @ mirai-vision/mirai-vision.github.io@7f0c4fc9188f3927f84a37ab5233cd953e2bc68b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -405,9 +405,9 @@ ninja.data = [{
           description: "Assistant Professor",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-manisha-verma",
+          title: 'Manisha Verma',
+          description: "Assistant Professor",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
